50f58ef4525b971ed3069305f5a6a50e252511e8
xamesLab <x2.0bgj@gmail.com>
2022-04-21 22:24:24 +0300

    test commit

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,6 @@ const app = express();
 const port = 5000;
 
 app.get("/", (request, response) => {
-    response.send("Hello world!");
+    response.send("Hello world!!!");
 });
 app.listen(port, () => console.log(`Running on port ${port}`));
