98dcfdb122421290edbd20a33dd50d1936c54b87
xamesLab <x2.0bgj@gmail.com>
2022-06-12 13:38:30 +0300

    ls

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+    const [test, setTest] = useState((localStorage.getItem("test"))||"a")
+
+    useEffect(()=>{
+        localStorage.setItem("test", test)
+    },[test])
+
     return (
         <div className="">
-            <header className="">test</header>
+            <header className="">test34</header>
+            <p>{test}</p>
+            <button onClick={()=>setTest(test+"a")}>click</button>
         </div>
     );
 }
