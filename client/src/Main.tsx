a67fb1f463e055fad79f9a9ffe8bbc117daeb30a
xamesLab <x2.0bgj@gmail.com>
2022-06-22 22:59:09 +0300

    clearing code

--- client/src/Main.tsx
+++ client/src/Main.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import MainHeader from "./components/MainHeader";
 import { useTypedSelector } from "./hooks/useTypedSelector";
-import GlobalStyles from "./styles/global";
 import Modal from "./UI/Modal";
-import { BtnBase, FAIcon, Loader } from "./styles/components";
+import {  FAIcon, Loader } from "./styles/components";
 import { faCog } from "@fortawesome/free-solid-svg-icons";
 
 function Main() {
     const { theme } = useTypedSelector((state) => state.themeReducer);
     const { users } = useTypedSelector((state) => state.usersReducer);
     const { user, loading, isAuth } = useTypedSelector((state) => state.userReducer);
+    
     const handler = () => {
         //dispatch(getUsers());
     };
@@ -25,26 +25,21 @@ function Main() {
 
     return (
         <div className={`main ${theme}`}>
-            <GlobalStyles />
             <Modal />
             <MainHeader />
-            {loading ? "loading..." : ""}
+            {loading ? <Loader /> : <></>}
             <div >
                 <div className="base">test theme scss {theme}</div>
             </div>
             <header className="">
-                {user.username || "user"} auth:{isAuth ? "is auth" : "not auth"}
+                {user.username || "user"} auth: {isAuth ? "is auth" : "not auth"}
             </header>
             <button onClick={handler}>test</button>
             <button onClick={handler2}>test-tets</button>
             {users.map((user) => (
                 <div key={user._id}>{user.username}</div>
             ))}
-            <BtnBase paddingLeftRight={1} paddingTopBottom={0.2}>
-                test btn
-            </BtnBase>
             <FAIcon icon={faCog} $animated />
-            <Loader />
         </div>
     );
 }
--- client/src/components/MainHeader.tsx
+++ client/src/components/MainHeader.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-import { useAppDispatch } from "../hooks/useTypedSelector";
+import { useAppDispatch, useTypedSelector } from "../hooks/useTypedSelector";
 import { setModalContent, toggleModal } from "../store/action-creators/modalActions";
 import { toggleTheme } from "../store/action-creators/themeActions";
 import "./MainHeader.scss";
 
 const MainHeader: React.FC = () => {
     const dispatch = useAppDispatch();
+    const { theme } = useTypedSelector((state) => state.themeReducer);
 
     const handlerTheme = () => {
         dispatch(toggleTheme());
@@ -22,7 +23,9 @@ const MainHeader: React.FC = () => {
                     <h2>LOGO</h2>
                 </div>
                 <div className="header__auth">
-                    <div className="header__profile header__btn"></div>
+                    <div className="header__profile header__btn">
+                        Profile
+                    </div>
                     <div className="header__login header__btn" onClick={() => handlerModal("Login")}>
                         Login
                     </div>
@@ -30,8 +33,8 @@ const MainHeader: React.FC = () => {
                         Register
                     </div>
                     <div className="header__logout header__btn">Logout</div>
-                    <div className="header__theme header__btn" onClick={handlerTheme}>
-                        toggle theme
+                    <div className="header__theme" onClick={handlerTheme}>
+                        {theme==='theme--default'?'L':'D'}
                     </div>
                 </div>
             </section>
